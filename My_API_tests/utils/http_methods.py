db3647afe2ad399a83603902214fdf699aef0039
Sergey Malinovskiy <malinovskiy.qa@gmail.com>
2023-08-14 20:54:43 +0300

    Добавления и добавление логирования к HTTP методам

--- My_API_tests/utils/http_methods.py
+++ My_API_tests/utils/http_methods.py
@@ -1,4 +1,5 @@
 import requests
+from utils.logger import Logger
 
 """Список HTTP методов"""
 
@@ -9,21 +10,29 @@ class HttpMethods:
 
     @staticmethod
     def get(url):
+        Logger.add_request(url, method="GET")
         result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method="POST")
         result = requests.post(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method="PUR")
         result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method="DELETE")
         result = requests.delete(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+        Logger.add_response(result)
         return result
 
